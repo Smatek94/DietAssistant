14982859dc3d95491140b5c3714939ee5fa9afa4
Mateusz <mateusz.skolimowski@toreforge.com>
2018-07-18 11:58:30 +0200

    MainActivity drawer

--- app/src/main/java/com/skolimowskim/dietassistant/MainActivity.kt
+++ app/src/main/java/com/skolimowskim/dietassistant/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.skolimowskim.dietassistant
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.skolimowskim.dietassistant.view.meals.MealsActivity
 import com.skolimowskim.dietassistant.view.products.ProductsActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +16,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(MealsActivity.createIntent(this))
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_launcher_background)
+        }
+
+        nav_view.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawer_layout.closeDrawers()
+            when(menuItem.itemId) {
+                R.id.nav_meals-> {
+                    startActivity(MealsActivity.createIntent(this))
+                }
+                R.id.nav_products -> {
+                    startActivity(ProductsActivity.createIntent(this))
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
